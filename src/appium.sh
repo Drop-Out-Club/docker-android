ab768a29f395f61dd1314d16add51271d8970ac2
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2021-03-26 16:20:55 -0700

    changed a comment that had a screen ratio as 9:19.5 to be 6:13, since it makes no sense to have a decimal within a ratio

--- src/appium.sh
+++ src/appium.sh
@@ -14,7 +14,7 @@ elif [[ "$SCREEN_WIDTH" == "1080" && "$SCREEN_HEIGHT" == "2020" ]]; then # ?:?? 
 elif [[ "$SCREEN_WIDTH" == "1080" && "$SCREEN_HEIGHT" == "1620" ]]; then # 2:3 1080:1620
 	sed 's/height 1920/height 1620/g' /root/devices/skins/nexus_5/layout > /root/devices/skins/nexus_5/layout.new
 	mv /root/devices/skins/nexus_5/layout.new /root/devices/skins/nexus_5/layout
-elif [[ "$SCREEN_WIDTH" == "1080" && "$SCREEN_HEIGHT" == "2340" ]]; then # 9:19.5 1080x2340
+elif [[ "$SCREEN_WIDTH" == "1080" && "$SCREEN_HEIGHT" == "2340" ]]; then # 6:13 1080x2340
 	sed 's/height 1920/height 2340/g' /root/devices/skins/nexus_5/layout > /root/devices/skins/nexus_5/layout.new
 	mv /root/devices/skins/nexus_5/layout.new.new /root/devices/skins/nexus_5/layout
 elif [[ "$SCREEN_WIDTH" == "540" && "$SCREEN_HEIGHT" == "1128" ]]; then # 9:20 540x1200 (useable space only on China S30)
